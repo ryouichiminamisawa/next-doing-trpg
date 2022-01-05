1ba703d86a69c4a9e2a8fbb9a3324e9d004ff6eb
ryouichi <mines104aka8901@outlook.jp>
2022-01-05 09:08:44 +0900

    いらないやつを消しました

--- pages/templates/CharactersList.jsx
+++ pages/templates/CharactersList.jsx
@@ -1,47 +1,45 @@
-import { initializeApp } from "firebase/app";
-import {
-	getDocs,
-	getFirestore,
-	query,
-	collection,
-	QuerySnapshot,
-} from "firebase/firestore";
-// import Link from "next/link";
-import React from "react";
-// import { render } from "react-dom";
-import { firebaseConfig } from "../../Firebase/firebaseConfig";
+// import { initializeApp } from "firebase/app";
+// import {
+// 	getDocs,
+// 	getFirestore,
+// 	query,
+// 	collection,
+// 	QuerySnapshot,
+// } from "firebase/firestore";
+// import React from "react";
+// import { firebaseConfig } from "../../Firebase/firebaseConfig";
 
-export default class Index extends React.Component {
-	static async getInitialProps() {
-		const app = initializeApp(firebaseConfig);
-		const db = getFirestore(app);
-		// console.log(collection())
-		const q = query(collection(db, "characters"));
-		const querySnapShot = await getDocs(q);
+// export default class Index extends React.Component {
+// 	static async getInitialProps() {
+// 		const app = initializeApp(firebaseConfig);
+// 		const db = getFirestore(app);
 
-		let data = [];
-		querySnapShot.forEach((doc) => {
-			data.push(Object.assign({ id: doc.id }, doc.data()));
-			return data;
-		});
-	}
+// 		const q = query(collection(db, "characters"));
+// 		const querySnapShot = await getDocs(q);
 
-	render() {
-		const firestoreDatas = this.props.datas;
-		return (
-			<div>
-				<h3>Firestoreのデータ一覧</h3>
-				<div>
-					<ul>
-						{firestoreDatas.map((characters) => (
-							<li>
-								<a>{characters.TRPG}</a>
-							</li>
-						))}
-					</ul>
-				</div>
-				<div></div>
-			</div>
-		);
-	}
-}
+// 		let data = [];
+// 		querySnapShot.forEach((doc) => {
+// 			data.push(Object.assign({ id: doc.id }, doc.data()));
+// 			return data;
+// 		});
+// 	}
+
+// 	render() {
+// 		const firestoreDatas = this.props.datas;
+// 		return (
+// 			<div>
+// 				<h3>Firestoreのデータ一覧</h3>
+// 				<div>
+// 					<ul>
+// 						{firestoreDatas.map((characters) => (
+// 							<li>
+// 								<a>{characters.TRPG}</a>
+// 							</li>
+// 						))}
+// 					</ul>
+// 				</div>
+// 				<div></div>
+// 			</div>
+// 		);
+// 	}
+// }
